4ac9e604ef906927fca82597440d14cce4e8a55f
david-liljewall <davidliljewall96@gmail.com>
2022-06-09 14:57:39 -0500

    final commit 20220609

--- controlScript.py
+++ controlScript.py
@@ -2,6 +2,7 @@ from asyncore import read
 from msilib import sequence
 import pyautogui
 import pandas as pd
+import readDepth
 
 def readClicks(clickfile):
     clicks = pd.read_csv(clickfile, names=['x','y'])
@@ -13,7 +14,14 @@ def clearEntry():
         pyautogui.hotkey('backspace')
 
 if __name__ == '__main__':
-    readClicks('sequence1.csv')
-    clearEntry()
-    readClicks('sequence2.csv')
-    clearEntry()
+    data = readDepth.readDepth('Depth.txt')
+    i = 0
+    for time in data['Time Window']:
+        if i == 0:
+            readClicks('sequence1.csv')
+            clearEntry()
+            pyautogui.typewrite(str(time[0]))
+            readClicks('sequence2.csv')
+            clearEntry()
+            pyautogui.typewrite(str(time[1]))
+            i = 1
--- readDepth.py
+++ readDepth.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
-data = pd.read_csv('Depth.txt',delim_whitespace=True)
-data['Actual Time'] = data['Cycle'] * data['Time']
-data['Time Window Lower Bound'] = data['Actual Time'] - 1
-data['Time Window Lower Bound'][0] = 0
-data['Time Window Upper Bound'] = data['Actual Time'] + 1
-data['Time Window'] = data[['Time Window Lower Bound','Time Window Upper Bound']].apply(tuple, axis=1)
-del data['Time Window Lower Bound']
-del data['Time Window Upper Bound']
+def readDepth(filename):
+    data = pd.read_csv(filename, delim_whitespace=True)
+    data['Actual Time'] = data['Cycle'] * data['Time']
+    data['Time Window Lower Bound'] = data['Actual Time'] - 1
+    data['Time Window Lower Bound'][0] = 0
+    data['Time Window Upper Bound'] = data['Actual Time'] + 1
+    data['Time Window'] = data[['Time Window Lower Bound','Time Window Upper Bound']].apply(tuple, axis=1)
+    del data['Time Window Lower Bound']
+    del data['Time Window Upper Bound']
+    return data
