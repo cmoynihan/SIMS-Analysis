cf43fddca6cdcc05a82c6b3b78ec51f8f69a6ed5
Ian Brown <ianbrown9016@gmail.com>
2022-06-13 15:39:23 -0500

    rename exporterGUI to recorderGUI and combine recorderGUI witclickRecorder

--- recorderGUI.py
+++ recorderGUI.py
@@ -2,11 +2,16 @@
 import tkinter as tk
 from tkinter import DISABLED, END, NORMAL, ttk
 from tkinter import messagebox
-import clickRecorder
 import pyautogui
 import pandas as pd
 from pynput import mouse
 import os
+
+pyautogui.FAILSAFE = True #DO NOT TURN OFF!! FAILSAFE allows the user to terminate the program by mousing up to the top left of the screen.
+pyautogui.PAUSE = 1 #delay (in seconds) between pyautogui calls
+recording = False #False if the program is not recording to the click list, True if it is
+clicks = [] #list to temporarily store locations of clicks during recording
+screenWidth, screenHeight = pyautogui.size()
 
 #GUI window class
 class exporterGUI(tk.Tk):
@@ -53,32 +58,33 @@ class exporterGUI(tk.Tk):
             #if a duplicate is found, ask the user if they would like to replace it
             replace = messagebox.askyesno(title="Replace saved file?", message='The filename you have input already exists. Do you want to replace it?')
             if replace:
-                clickRecorder.recording = True #begin recording
+                global recording
+                recording = True #begin recording
                 self.Stop['state'] = NORMAL #make the "Stop" button not grey
         else:
-            clickRecorder.recording = True #begin recording
+            recording = True #begin recording
             self.Stop['state'] = NORMAL #make the "Stop" button not grey
     
     #stop button binding. Ends recording
     def stop(self):
         filename = self.entry.get() + '.csv'
-
+        global clicks, recording
         try:
-
-            if clickRecorder.recording == True:
-                clickRecorder.write_clicks(clickRecorder.clicks, filename) #write click recording to a file
-            clickRecorder.recording = False #turn off click recording
-            clickRecorder.clicks = [] #clear list that temporarily stores clicks
+            if recording == True:
+                write_clicks(clicks, filename) #write click recording to a file
+                print('success')
+            clicks = [] #clear list that temporarily stores clicks
         except:
             messagebox.showerror(title='Error', message='Recording was not saved. Make sure that the .csv file is not open.')
+        recording = False #turn off click recording
         self.Stop['state'] = DISABLED #grey out stop button
         
     #play button binding. Plays back recording with the filename indicated in the Entry box
     def play(self):
         try:
-            clicks = pd.read_csv(self.entry.get() + '.csv', names=['x','y', 'button']) #read clicks from file into a dataframe
+            clicklist = pd.read_csv(self.entry.get() + '.csv', names=['x','y', 'button']) #read clicks from file into a dataframe
             #iterate through the rows of the dataframe to extract click locations
-            for index, row in clicks.iterrows():
+            for index, row in clicklist.iterrows():
                 #left or right click based on input
                 if row['button'] == 'Button.left':
                     pyautogui.click(row['x'], row['y'])
@@ -87,9 +93,22 @@ class exporterGUI(tk.Tk):
         except:
             messagebox.showerror("Error","Please input a valid filename! (.csv file extension automatically added)")
 
+#write clicks to a .csv file
+def write_clicks(clickArray, filename):
+    # print('writing,', clickArray[1:-2])
+    with open(str(filename),'w') as clickList:
+        #selects a subset of clicks that excludes the first and last two clicks in the series
+        for click in clickArray[1:-2]:
+            clickList.write(f'{click[0]},{click[1]},{click[2]}\n')
+
+def on_click(x, y, button, pressed):
+    if pressed and recording:
+        clicks.append((x,y, button))
+        print(x,',',y, ',',button)
+
 if __name__ == '__main__':
     #start listening for clicks
-    mouse_listener = mouse.Listener(on_click=clickRecorder.on_click)
+    mouse_listener = mouse.Listener(on_click=on_click)
     mouse_listener.start()
     root = exporterGUI()
     root.mainloop()
